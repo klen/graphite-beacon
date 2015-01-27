38aa65c0bc46f1c4827a2e1a15a452f0ba5d27e1
Cody Soyland <cody@soyland.org>
2015-01-27 17:42:20 -0600

    Add Opsgenie handler

---
+++ graphite_beacon/handlers/opsgenie.py
@@ -0,0 +1,45 @@
+from . import AbstractHandler
+from tornado import gen, httpclient
+import json
+import urllib
+
+
+class OpsgenieHandler(AbstractHandler):
+
+    name = 'opsgenie'
+
+    defaults = {
+        'api_key': None
+    }
+
+    def init_handler(self):
+        self.api_key = self.options.get('api_key')
+        assert self.api_key, "Opsgenie API key not defined."
+        self.client = httpclient.AsyncHTTPClient()
+
+    @gen.coroutine
+    def notify(self, level, alert, value, target=None, *args, **kwargs):
+
+        message = self.get_short(level, alert, value, target, *args, **kwargs)
+        description = "{url}/composer/?{params}".format(
+                        url=self.reactor.options['graphite_url'],
+                        params=urllib.urlencode({'target': alert.query}))
+        alias = target + ':' + alert.name
+
+        if level == 'critical':
+            yield self.client.fetch(
+                    'https://api.opsgenie.com/v1/json/alert',
+                    method='POST',
+                    headers={'Content-Type': 'application/json'},
+                    body=json.dumps({'apiKey': self.api_key,
+                                     'message': message,
+                                     'alias': alias,
+                                     'description': description}))
+        elif level == 'normal':
+            # Close issue
+            yield self.client.fetch(
+                    'https://api.opsgenie.com/v1/json/alert/close',
+                    method='POST',
+                    headers={'Content-Type': 'application/json'},
+                    body=json.dumps({'apiKey': self.api_key, 'alias': alias}))
+        # TODO: Maybe add option to create alert when level == 'warning'?
