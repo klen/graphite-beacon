3c2501e3f99cf83d35f761fa60b15ee5b198b557
Abraham Kang <kidskosta@gmail.com>
2015-07-01 09:06:42 -0700

    debug

--- graphite_beacon/alerts.py
+++ graphite_beacon/alerts.py
@@ -160,7 +160,7 @@ class BaseAlert(_.with_metaclass(AlertFabric)):
             else:
                 self.notify('normal', value, target, rule=rule)
             LOGGER.info("work: %s pasthour: %s recorded: %s", work, self.pastHour, self.recorded)
-            print historicValues
+            print self.historicValues
             # INSERT DAILY STUFF HERE #
             if work and not value is None and target in self.historicValues:
                 conn = psycopg2.connect(self.reactor.options.get('database'))
